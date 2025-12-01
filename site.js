4fab01cd2312962c0542b360389e7b00e71b8613
zahrevans <526zevans@frhsd.com>
2025-12-01 09:01:41 -0500

    three

--- site.js
+++ site.js
@@ -21,22 +21,26 @@
 */
 
 
+
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+            return {
+                  // This holds your movies.json data.
+                  movies: [],
+                  title: "",
+                  owner: "Zahr Evans",
+                  github: "https://github.com/zahrevans/NJIT-3_StarterFiles",
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+
+            }
+      },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
       }
